78afbcf59840bee315e6b8397277ff880b5173f2
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2022-05-27 01:38:46 +0900

    Add publishing settings for jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    `maven-publish`
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "dev.cube1"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -13,4 +14,16 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.exposed:exposed-dao:0.38.2")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.38.2")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.cube1"
+            artifactId = project.name
+            version = project.version as String
+
+            from(components["java"])
+        }
+    }
 }
